d86b5305dcbf1064768097af83d7cc88d0807a76
Stas <botnarstasyan112002@gmail.com>
2020-11-18 01:09:34 +0200

    dumb

---
+++ script4.js
@@ -0,0 +1,4 @@
+let randomNumbers = Math.floor(Math.random() * (100-10) + 10);
+let CatchNumber = Number(prompt('Enter your lucky number on 10 to 100'));
+
+console.log(randomNumbers=CatchNumber, 'You are win!' );
