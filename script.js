95f95b16aaaa764d23ea9fd48c48949e75cbcde7
Stas <botnarstasyan112002@gmail.com>
2020-11-18 00:26:11 +0200

    home2

---
+++ script.js
@@ -0,0 +1,6 @@
+let firtsNum = Number(prompt('Enter 1st number'));
+let secondNum = Number(prompt('Enter 2nd number'));
+let thirdNum = Number(prompt('Enter 3rd number'));
+
+let average = (firtsNum + secondNum + thirdNum)/3;
+console.log(average);
