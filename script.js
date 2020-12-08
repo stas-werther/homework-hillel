18fd6ba20cd1b9152ce4ca0e9034bbcd6810c9e2
Stas <botnarstasyan112002@gmail.com>
2020-12-08 18:59:38 +0200

    magisc

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
-let firtsNum = Number(prompt('Enter 1st number'));
-let secondNum = Number(prompt('Enter 2nd number'));
-let thirdNum = Number(prompt('Enter 3rd number'));
+const magicBook = {
+    recipes: {
+        golem: {
+            bone: 10,
+            stone: 100,
+            nail: 100,
+            poo: 53
+        },
+        littleDevil: {
+            lava: 10,
+            evil: 999
+        },
+    },
+};
 
-let average = (firtsNum + secondNum + thirdNum)/3;
-console.log(average);
+const kitchen = {
+    bone: 999,
+    stone: 999,
+    nail: 999,
+    poo: 999,
+    lava: 999,
+    evil: 999,
+    kettle: { 
+        material: 'metal',
+        ingridients: {},
+    },
+    addIngridientToKettle(nameOfIngridient, amount) {
+        this[nameOfIngridient] -= amount;
+        this.kettle.ingridients[nameOfIngridient] =
+        this.kettle.ingridients[nameOfIngridient || 0] + amount;
+    },
+    cook(recipe) {
+        let couldBeCooked = true;
+        for(let key in recipe) {
+            const neededAmountOfIngridients = recipe[key];
+            if (
+                !this.kettle.ingridients[key] ||
+                this.kettle.ingridients[key] < neededAmountOfIngridients
+            ) {
+                couldBeCooked = false;
+                break;
+            }
+        }
+        
+        if (couldBeCooked) {
+            console.log('Could be cooked');
+        } else {
+            console.log('Not enugh ingridients');
+        }
+    },
+};
+
+kitchen.addIngridientToKettle('bone', 10);
+kitchen.addIngridientToKettle('stone', 100);
+kitchen.addIngridientToKettle('nail', 100);
+kitchen.addIngridientToKettle('poo', 53);
+
+kitchen.cook(magicBook.recipes.golem);
