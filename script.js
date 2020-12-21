874363aab3cb962cf8bbdef932f490dce0b01049
Stas <botnarstasyan112002@gmail.com>
2020-12-21 17:40:58 +0200

    hw9

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-let firtsNum = Number(prompt('Enter 1st number'));
-let secondNum = Number(prompt('Enter 2nd number'));
-let thirdNum = Number(prompt('Enter 3rd number'));
+let calculations = {
+    salarySum: function() {
+      return this.reduce(function(acc, next) {
+        return acc + next;
+      }, 0);
+    },
+    logName: function() {
+      this.forEach(function(user) {
+        console.log(user.name);
+      });
+    },
+    findUsersByIds: function(ids) {
+      return this.filter(function(user) {
+        return ids.find(function(id) {
+            return user.id === id;
+        })
+      });
+    }
+  };
+  
+  let users = [
+    {
+      id: 1,
+      name: "User 2",
+      salary: 200
+    },
+    {
+      id: 2,
+      name: "User 3",
+      salary: 200
+    },
+    {
+      id: 3,
+      name: "User 4",
+      salary: 200
+    }
+  ];
+ calculations.logName.call(users);
+ 
+ let salaryUsers = users.map((element) => element.salary);
+ console.log(calculations.salarySum.call(salaryUsers));
 
-let average = (firtsNum + secondNum + thirdNum)/3;
-console.log(average);
+ console.log(calculations.findUsersByIds.call(users, [1,2,3]));
