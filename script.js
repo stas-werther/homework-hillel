fa9132bf9e84770abdf0f74552ca749785401d28
Stas <botnarstasyan112002@gmail.com>
2020-12-25 19:58:58 +0200

    hw11

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let firtsNum = Number(prompt('Enter 1st number'));
-let secondNum = Number(prompt('Enter 2nd number'));
-let thirdNum = Number(prompt('Enter 3rd number'));
-
-let average = (firtsNum + secondNum + thirdNum)/3;
-console.log(average);
+/*const Singleton = (() => {
+    const uniqueKey = Symbol('instance');
+    let instance;
+    function Singleton(key) {
+      if(key !== uniqueKey) throw new Error('You should you instance static method instead');
+    }
+   
+   Singleton.getInstance = () => {
+     return instance ? instance: instance = new Singleton(uniqueKey);
+   }
+   
+   return Singleton;
+ })();
+ 
+ console.log(Singleton.getInstance() === Singleton.getInstance())*/
--- script2.js
+++ script2.js
@@ -1,2 +1,24 @@
-let YearUser = Number(prompt("Enter your year of birth"));
-console.log(2020 - YearUser);
+'use strict';
+
+const weapon = {
+	modifications: ['total power', 'giant bullet'],
+    type: ['boom'],
+    [Symbol.iterator]() {
+        const values = Object.values(weapon);
+        return {
+            next() {
+                const done = !values.length;
+                const nextValue = values.shift();
+                
+                return {
+                    value: nextValue,
+                    done,
+                };
+            },
+        };
+    },
+};
+
+for (let key of weapon) {
+    console.log(key);
+}
