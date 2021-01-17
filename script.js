d9ffe58741d10158b1abb75ea20e71e1baba8c23
Stas <botnarstasyan112002@gmail.com>
2021-01-17 18:18:33 +0200

    hw14

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-let firtsNum = Number(prompt('Enter 1st number'));
-let secondNum = Number(prompt('Enter 2nd number'));
-let thirdNum = Number(prompt('Enter 3rd number'));
-
-let average = (firtsNum + secondNum + thirdNum)/3;
-console.log(average);
+function MilitaryResource(type, health, distance) {
+    this.type = type;
+    this.health = this.maxHealth = health;
+    this.distance = this.maxDistance = distance;
+ }
+ 
+ MilitaryResource.prototype.isReadyToMove = (distance) => {
+    if(distance > 10 && distance <= 50) {
+        return true;
+    } else {
+        return false;
+    }
+ };
+ MilitaryResource.prototype.isReadyToFight = (health) => {
+     if(health >= 25) {
+         return true;
+     } else {
+         return false;
+     }
+ };
+ MilitaryResource.prototype.restore;
+ MilitaryResource.prototype.clone;
+ 
+ 
+ function Squad(defaultResources) {
+    this.squad = [];
+    if (defaultResources) this.combineResources(defaultResources);
+ }
+ Squad.prototype = Object.create(MilitaryResource.prototype);
+ Squad.prototype.constructor = Squad;
+ const gun = new Squad();
+ console.log(gun);
+ 
+ 
+ Squad.prototype.isReadyToMove;
+ Squad.prototype.isReadyToFight;
+ Squad.prototype.restore;
+ Squad.prototype.getReadyToMoveResources;
+ Squad.prototype.combineResources;
+ Squad.prototype.clone;
