3e41fe8dfbf3149a54f7662dd1121ee7b032fedc
Stas <botnarstasyan112002@gmail.com>
2021-01-18 20:42:50 +0200

    hw14-02

--- script.js
+++ script.js
@@ -6,20 +6,31 @@ function MilitaryResource(type, health, distance) {
  
  MilitaryResource.prototype.isReadyToMove = (distance) => {
     if(distance > 10 && distance <= 50) {
+        console.log('Готов двигаться!');
         return true;
     } else {
+        console.log('Движение невозможно...');
         return false;
     }
  };
  MilitaryResource.prototype.isReadyToFight = (health) => {
      if(health >= 25) {
+        console.log('К бою готов :)');
          return true;
      } else {
+         console.log('Слишком мало хп для сражение (');
          return false;
      }
  };
- MilitaryResource.prototype.restore;
- MilitaryResource.prototype.clone;
+ MilitaryResource.prototype.restore = (maxHealth, maxDistance) => {
+    if((health = 0) && (distance = 0)) {
+        health =+ maxHealth;
+        distance =+ maxDistance;
+    };
+ };
+ MilitaryResource.prototype.clone = (oneSquad, copy) => {
+    copy = Object.assign({}, oneSquad);
+ };
  
  
  function Squad(defaultResources) {
