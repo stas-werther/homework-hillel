029d5e3862587be5eb5321f10e50e96770dc155c
Stas <botnarstasyan112002@gmail.com>
2021-01-22 17:30:37 +0200

    hw15-updating

--- script.js
+++ script.js
@@ -1,70 +1,66 @@
-class ControlPanel {
-
-  isReadyToMove() {
-    if (this.distance) {
-          return this.distance > 0;
-    } else if (this.squad) {
-          return this.squad.every((item) => item.isReadyToMove());
-    } throw "it is not MilitaryResource";
-  };
-  
-  isReadyToFight() {
-    if (this.health) {
-          return this.health > 0;
-    } else if (this.squad) {
-          return this.squad.every((item) => item.isReadyToFight());
-    } throw "it is not MilitaryResource";
-  };
-  
-  restore() {
-    this.restoreHealth();
-    this.restoreMove();
-  };
-  
-  restoreHealth() {
-    if (this.health) {
-          this.health = this.maxHealth;
-    } else if (this.squad) {
-          this.squad.forEach((item) => item.restoreHealth());
-    }
-  };
-  
-  restoreMove() {
-    if (this.distance) {
-        this.distance = this.maxDistance;
-    } else if (this.squad)
-      this.squad.forEach((item) => item.restoreMove());
-  };
-  
-  clone() {
-     const result = Object.create(
-       Object.getPrototypeOf(this),
-       Object.getOwnPropertyDescriptors(this)
-      );
-  
-     if (result.squad) {
-          result.squad = this.squad.map((element) => element.clone());
-     }
-     return result;
-  }
-}
-
-class MilitaryResource extends ControlPanel {
+class MilitaryResource {
     constructor(type, health, distance) {
-        super();
         this.type = type;
         this.health = this.maxHealth = health;
         this.distance = this.maxDistance = distance;
     }
+
+    isReadyToMove() {
+      return this.distance > 0;
+    }
+
+    isReadyToFight() {
+      return this.health > 0;
+    }
+
+    restore() {
+      this.restoreHealth();
+      this.restoreMove();
+    }
+
+    restoreHealth() {
+      this.health = this.maxHealth;
+    }
+
+    restoreMove() {
+      this.distance = this.maxDistance;
+    }
+
+    clone() {
+      return new MilitaryResource(this.type, this.health, this.distance);
+    }
 }
-class Squad extends ControlPanel {
+class Squad {
   constructor(defaultResources) {
-      super();
       if (defaultResources) this.combineResources(defaultResources);
   }
 
   squad = [];
 
+  isReadyToMove() {
+    this.squad.every((item) => item.isReadyToMove());
+  }
+
+  isReadyToFight() {
+    this.squad.every((item) => item.isReadyToFight());
+  }
+
+  restore() {
+    this.restoreHealth();
+    this.restoreMove();
+  }
+
+  restoreHealth() {
+    this.squad.forEach((item) => item.restoreHealth());
+  }
+
+  restoreMove() {
+    this.squad.forEach((item) => item.restoreMove());
+  }
+
+  clone() {
+    return new Squad(this.squad.map((element) => element.clone()));
+  }
 
   getReadyToMoveResources() {
     return this.squad.map((element) => element.isReadyToMove && element);
