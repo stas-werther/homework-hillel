f8dad7540e1d8f1fded25cdce0b3b3050b427f6e
Stas <botnarstasyan112002@gmail.com>
2021-02-02 19:12:00 +0200

    hw17

--- script4.js
+++
@@ -1,4 +0,0 @@
-let randomNumbers = Math.floor(Math.random() * (100-10) + 10);
-let CatchNumber = Number(prompt('Enter your lucky number on 10 to 100'));
-
-console.log(randomNumbers=CatchNumber, 'You are win!' );
